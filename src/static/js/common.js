4f4478b1080d559b94d555b61de380b661c8a284
Aslan <berezg86@gmail.com>
2020-02-28 02:41:23 +0300

    Шапка, футер, блок шагов доставки, слайдер на главной

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -12,30 +12,45 @@ $(document).ready(function() {
         }
     });
 
-    $('.cat-btn').click(function(){
+    $('.cat-btn').click(function() {
         $('.cat-btn__list').fadeToggle();
     });
 
 });
 
-var galleryThumbs = new Swiper('.gallery-thumbs', {
-      spaceBetween: 10,
-      slidesPerView: 4,
-      loop: true,
-      freeMode: true,
-      loopedSlides: 5, //looped slides should be the same
-      watchSlidesVisibility: true,
-      watchSlidesProgress: true,
-    });
-    var galleryTop = new Swiper('.gallery-top', {
-      spaceBetween: 10,
-      loop:true,
-      loopedSlides: 5, //looped slides should be the same
-      navigation: {
+var mainSlider = new Swiper('.main-slider', {
+    spaceBetween: 0,
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-      },
-      thumbs: {
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    effect: 'fade'
+});
+
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    loop: true,
+    freeMode: true,
+    loopedSlides: 5, //looped slides should be the same
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+});
+var galleryTop = new Swiper('.gallery-top', {
+    spaceBetween: 10,
+    loop: true,
+    loopedSlides: 5, //looped slides should be the same
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
         swiper: galleryThumbs,
-      },
-    });
+    },
+});
