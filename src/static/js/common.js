585e9d9cd20972d7433a7469b29312f4ba0d41d0
Aslan <berezg86@gmail.com>
2020-02-27 21:29:24 +0300

    Тест пуш

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,4 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function() {
     // font-family: 'Ubuntu';
     // font-family: 'Roboto';
+
+    $('.burger').click(function() {
+        $('.main-nav').fadeToggle();
+    });
+    
+    $(window).resize(function() {
+        if (window.matchMedia("(min-width: 992px)").matches) {
+            $('.main-nav').removeAttr('style');
+        }
+    });
+
 });
