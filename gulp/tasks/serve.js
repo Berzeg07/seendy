6f40736250a70c5a993fa0e1d540c4dd3aa9e18c
betokov <betokov93@mail.ru>
2020-02-29 01:33:02 +0300

    разметка страницы регистрация

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -3,7 +3,7 @@ module.exports = function() {
         $.bs.init({
             server: {
                 baseDir: "./build",
-                index: "success.html"
+                index: "registration.html"
             }
         });
     });
