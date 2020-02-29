ef06bfeddd7e67c0bfc629228ca9df3780398304
Aslan <berezg86@gmail.com>
2020-02-29 15:30:39 +0300

    Верстка главной

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -6,9 +6,36 @@ $(document).ready(function() {
         $('.main-nav').fadeToggle();
     });
 
+
+
+    $(function($) {
+        $(document).click(function(event) {
+            if ($(event.target).closest(".cat-btn").length)
+            return;
+            if ($(event.target).closest(".cat-btn__list").length)
+            return;
+            $(".cat-btn__list").fadeOut();
+            event.stopPropagation();
+        });
+    });
+
+    $(function($) {
+        $(document).click(function(event) {
+            if ($(event.target).closest(".burger").length)
+            return;
+            if ($(event.target).closest(".nav").length)
+            return;
+            $(".nav").fadeOut();
+            event.stopPropagation();
+        });
+    });
+
     $(window).resize(function() {
         if (window.matchMedia("(min-width: 992px)").matches) {
             $('.main-nav').removeAttr('style');
+            $('.main-nav').removeClass('nav');
+        }else{
+            $('.main-nav').addClass('nav');
         }
     });
 
@@ -16,47 +43,79 @@ $(document).ready(function() {
         $('.cat-btn__list').fadeToggle();
     });
 
+    $('.js-example-basic-single').select2();
 
-        $('.js-example-basic-single').select2();
-        $('.favor').click(function(){
-            $(this).toggleClass('active');
-        });
+    $('.favor').click(function() {
+        $(this).toggleClass('active');
+    });
+
+    var mainSlider = new Swiper('.brends-slider', {
+        spaceBetween: 0,
+        slidesPerView: 6,
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            499: {
+                slidesPerView: 2,
+                spaceBetweenSlides: 30
+            },
+            767: {
+                slidesPerView: 3,
+                spaceBetweenSlides: 40
+            },
+            800: {
+                slidesPerView: 5,
+                spaceBetweenSlides: 40
+            }
+        }
+    });
+
+    var mainSlider = new Swiper('.main-slider', {
+        spaceBetween: 0,
+        slidesPerView: 1,
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        effect: 'fade'
+    });
+
+
 
 });
 
-var mainSlider = new Swiper('.main-slider', {
-    spaceBetween: 0,
-    slidesPerView: 1,
-    loop: true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    effect: 'fade'
-});
 
-var galleryThumbs = new Swiper('.gallery-thumbs', {
-    spaceBetween: 10,
-    slidesPerView: 4,
-    loop: true,
-    freeMode: true,
-    loopedSlides: 5, //looped slides should be the same
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true,
-});
-var galleryTop = new Swiper('.gallery-top', {
-    spaceBetween: 10,
-    loop: true,
-    loopedSlides: 5, //looped slides should be the same
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    thumbs: {
-        swiper: galleryThumbs,
-    },
-});
+
+// var galleryThumbs = new Swiper('.gallery-thumbs', {
+//     spaceBetween: 10,
+//     slidesPerView: 4,
+//     loop: true,
+//     freeMode: true,
+//     loopedSlides: 5, //looped slides should be the same
+//     watchSlidesVisibility: true,
+//     watchSlidesProgress: true,
+// });
+// var galleryTop = new Swiper('.gallery-top', {
+//     spaceBetween: 10,
+//     loop: true,
+//     loopedSlides: 5, //looped slides should be the same
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+//     thumbs: {
+//         swiper: galleryThumbs,
+//     },
+// });
