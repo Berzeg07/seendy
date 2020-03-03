cf640951fcf45a859aa629e75e3b79a611ae4ed4
Aslan <berezg86@gmail.com>
2020-03-03 16:01:12 +0300

    мелкеи правки клиента

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -3,7 +3,7 @@ module.exports = function() {
         $.bs.init({
             server: {
                 baseDir: "./build",
-                index: "catalog.html"
+                index: "index.html"
             }
         });
     });
